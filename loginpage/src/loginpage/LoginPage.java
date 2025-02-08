1ba7595d0731991526436ce2ecb9593187bed90c
Ferhan <nuranferhan@gmail.com>
2025-02-08 20:54:50 +0300

    editing

--- loginpage/src/loginpage/LoginPage.java
+++ loginpage/src/loginpage/LoginPage.java
@@ -6,7 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader; // Düzeltildi ✅
+import javafx.fxml.FXMLLoader; 
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
