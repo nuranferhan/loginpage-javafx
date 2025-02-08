6bb60a563e2ce9ba78f876067aa82c362acfb9a6
Ferhan <nuranferhan@gmail.com>
2025-02-08 20:23:03 +0300

    editing

--- loginpage/src/loginpage/LoginPage.java
+++ loginpage/src/loginpage/LoginPage.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package loginpage;
 
 import java.awt.event.MouseEvent;
@@ -11,41 +6,38 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader; // Düzeltildi ✅
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-/**
- *
- * @author Lenovo
- */
 public class LoginPage extends Application {
     
     private double x = 0;
     private double y = 0;
+    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml")); 
         Scene scene = new Scene(root);
-        root.setOnMousePressed((MouseEvent event) ->{
+        
+        root.setOnMousePressed((MouseEvent event) -> {
             x = event.getSceneX();
             y = event.getSceneY();
-            
         });
-        
-        root.setOnMouseDragged((MouseEvent event) ->{
-            stage.setX(event.getScreenx() - x);
+
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - x); 
             stage.setY(event.getScreenY() - y);
-            
-                  
-    });
+        });
+
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    } 
 
     public static void main(String[] args) {
         launch(args);
     }
-    
-}
 }
